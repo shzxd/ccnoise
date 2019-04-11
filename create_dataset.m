cae3c0511e338f80670190d51f1221e6958b6688
shzxd <xiudongde@outlook.com>
2019-04-11 14:30:53 +0800

    添加注释

--- create_dataset.m
+++ create_dataset.m
@@ -1,5 +1,7 @@
 function create_dataset(row, col, prefix, start_idx, end_idx, postfix, savepath)
+% 求n幅图像的均值图像
 img_mean = make_mean(row, col, prefix, start_idx, end_idx, postfix);
+% 求n幅图像的协方差矩阵的平均协方差矩阵
 img_cov = make_cov(prefix, start_idx, end_idx, postfix, img_mean);
 img_mask = make_mask(prefix, start_idx, end_idx, postfix, 0.05, img_mean);
 img_mean = im2uint8(img_mean);
--- make_cov.m
+++ make_cov.m
@@ -1,12 +1,12 @@
 function covar = make_cov(prefix, start_idx, end_idx, postfix, img_mean)
-img_mean = img_mean * 255;
+img_mean = img_mean * 255;  %？
 covar = zeros(size(img_mean, 1), size(img_mean, 2), 6);
 for i = start_idx:end_idx
     img = double(imread(strcat(prefix, sprintf('%06d', i), postfix)));
-    covar(:, :, 1) = covar(:, :, 1) + (img(:, :, 1) - img_mean(:, :, 1)).^2;
+    covar(:, :, 1) = covar(:, :, 1) + (img(:, :, 1) - img_mean(:, :, 1)).^2; % 自协方差
     covar(:, :, 2) = covar(:, :, 2) + (img(:, :, 2) - img_mean(:, :, 2)).^2;
     covar(:, :, 3) = covar(:, :, 3) + (img(:, :, 3) - img_mean(:, :, 3)).^2;
-    covar(:, :, 4) = covar(:, :, 4) + (img(:, :, 1) - img_mean(:, :, 1)) .* (img(:, :, 2) - img_mean(:, :, 2));
+    covar(:, :, 4) = covar(:, :, 4) + (img(:, :, 1) - img_mean(:, :, 1)) .* (img(:, :, 2) - img_mean(:, :, 2)); % 互协方差
     covar(:, :, 5) = covar(:, :, 5) + (img(:, :, 1) - img_mean(:, :, 1)) .* (img(:, :, 3) - img_mean(:, :, 3));
     covar(:, :, 6) = covar(:, :, 6) + (img(:, :, 2) - img_mean(:, :, 2)) .* (img(:, :, 3) - img_mean(:, :, 3));
 end
